a565627de48e40425758ddf299d5467555770299
Nicoou <nf9600362@gmail.com>
2023-07-06 11:15:32 -0700

    task 3

--- models/base_model.py
+++ models/base_model.py
@@ -1,17 +1,30 @@
 #!/usr/bin/python3
+#generate a unique identifier for enyone class
 import uuid
 from datetime import datetime
 
-class BaseModel:
-    ""BaseModel""
+class BaseModel():
+    """BaseModel"""
     
-    def __init__(self, *args, **kwargs):
-        ""Constructor""
+    def __init__(self):
+        """Constructor"""
+        #generate a identifier unique and convert in the string
+        self.id = str(uuid.uuid4())
+        #get the date using datatime.now
+        self.created_at = datetime.now()
+        #update the date
+        self.updated_at = self.created_at
 
-        if kwargs:
-            for key,value in kwargs.items():
-                pass
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = self.created_at
+    def __str__(self):
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+    
+    def save(self):
+        self.updated_at = datetime.now()
+
+    # return a dictionary with all the values of the instance
+    def to_dict(self):
+        obj_dict = self.__dict__.copy()
+        obj_dict[' __class__ '] = self.__class__.__name__
+        obj_dict[' create_at '] = self.create_at.isoformat()
+        obj_dict[' update_at '] = self.update_at.isoformat()
+        return obj_dict
