fa0a3f265e46f27a8c044c90625fd6f8719c2f0c
Nicoou <nf9600362@gmail.com>
2023-07-07 05:35:26 -0700

    add description of all def

--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Base model"""
-import uuid
+from uuid import uuid4
 from datetime import datetime
 
 
@@ -10,20 +10,23 @@ class BaseModel():
     def __init__(self):
         """Constructor"""
         # generate a identifier unique and convert in the string
-        self.id = str(uuid.uuid4())
+        self.id = str(uuid4())
         # get the date using datatime.now
         self.created_at = datetime.now()
         # update the date
         self.updated_at = datetime.now()
 
     def __str__(self):
+        """string representation"""
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
+        """updated_at update the date"""
         self.updated_at = datetime.now()
 
     # return a dictionary with all the values of the instance
     def to_dict(self):
+        """dictionary contain all keys/values of the instance"""
         dict_aux = self.__dict__.copy()
         dict_aux['__class__'] = self.__class__.__name__
         dict_aux['created_at'] = self.created_at.isoformat()
