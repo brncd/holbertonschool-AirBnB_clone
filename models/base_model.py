74803bc2433012263e8585bbfe6eaa79e1a0c392
Nicoou <nf9600362@gmail.com>
2023-07-07 05:49:23 -0700

    test

--- models/base_model.py
+++ models/base_model.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Base model"""
 import uuid
-import datetime
+ import datetime
 
 
 class BaseModel():
@@ -12,23 +12,22 @@ class BaseModel():
         # generate a identifier unique and convert in the string
         self.id = str(uuid.uuid4())
         # get the date using datatime.now
-        self.created_at = datetime.datetime.now()
+        self.created_at = datetime.now()
         # update the date
-        self.updated_at = datetime.datetime.now()
+        self.updated_at = datetime.now()
 
     def __str__(self):
         """string representation"""
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
-        """update date with te current datetime"""
-        self.updated_at = datetime.datetime.now()
+        """update date to the update_at"""
+        self.updated_at = datetime.now()
 
-    # return a dictionary with all the values of the instance
     def to_dict(self):
-        """return a dictionary contain all keys/value"""
+        """return a dictionary contain all keys/values"""
         dict_aux = self.__dict__.copy()
-        dict_aux['__class__'] = str(type(self)).__name__
+        dict_aux['__class__'] = self.__class__.__name__
         dict_aux['created_at'] = self.created_at.isoformat()
         dict_aux['updated_at'] = self.updated_at.isoformat()
         return dict_aux
