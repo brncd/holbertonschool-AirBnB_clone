eb59053ec5010dd37219780b977b088c51bf8162
Nicoou <nf9600362@gmail.com>
2023-07-07 05:26:25 -0700

    fix to_dict names and pycodestyle

--- models/base_model.py
+++ models/base_model.py
@@ -1,30 +1,31 @@
 #!/usr/bin/python3
-#generate a unique identifier for enyone class
+# generate a unique identifier for enyone class
 import uuid
 from datetime import datetime
 
+
 class BaseModel():
     """BaseModel"""
-    
+
     def __init__(self):
         """Constructor"""
-        #generate a identifier unique and convert in the string
+        # generate a identifier unique and convert in the string
         self.id = str(uuid.uuid4())
-        #get the date using datatime.now
+        # get the date using datatime.now
         self.created_at = datetime.now()
-        #update the date
+        # update the date
         self.updated_at = self.created_at
 
     def __str__(self):
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
-    
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+
     def save(self):
         self.updated_at = datetime.now()
 
     # return a dictionary with all the values of the instance
     def to_dict(self):
-        obj_dict = self.__dict__.copy()
-        obj_dict[' __class__ '] = self.__class__.__name__
-        obj_dict[' create_at '] = self.create_at.isoformat()
-        obj_dict[' update_at '] = self.update_at.isoformat()
-        return obj_dict
+        dict_aux = self.__dict__.copy()
+        dict_aux['__class__'] = self.__class__.__name__
+        dict_aux['created_at'] = self.created_at.isoformat()
+        dict_aux['updated_at'] = self.updated_at.isoformat()
+        return dict_aux
