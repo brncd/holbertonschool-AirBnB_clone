217ae40ce060d4738300b7a280a4412549303ad0
Nicoou <nf9600362@gmail.com>
2023-07-07 06:02:52 -0700

    task 3 test

--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Base model"""
-import uuid
+from uuid import uuid4
 from datetime import datetime
 
 
@@ -8,9 +8,9 @@ class BaseModel():
     """class BaseModel"""
 
     def __init__(self):
-        """Constructor"""
+        """initialize values"""
         # generated a identifier unique and convert in the string
-        self.id = str(uuid.uuid4())
+        self.id = str(uuid4())
         # get the date using datatime.now
         self.created_at = datetime.now()
         # update the date
