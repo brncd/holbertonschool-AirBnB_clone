8a4f47e83cbe4b50dea5612cf111c018102983b4
Nicoou <nf9600362@gmail.com>
2023-07-08 08:57:57 -0700

    task 8 no complete only clas and test user

---
+++ tests/test_models/test_user.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" test User model """
+import unittest
+from models.user import User
+
+class BasemodelClass(unittest.TestCase):
+    """test basemodel"""
+    def test_UserEmail(self):
+        inst = User()
+        self.assertEqual(inst.email, '')
+        self.assertEqual(inst.password, '')
+        self.assertEqual(inst.first_name, '')
+        self.assertEqual(inst.last_name, '')
