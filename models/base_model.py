52f3d513203bd981ea69ac42187d91e9ddcdab1e
Nicoou <nf9600362@gmail.com>
2023-07-09 13:25:17 -0700

    rev fix basemodel

--- models/base_model.py
+++ models/base_model.py
@@ -11,23 +11,20 @@ class BaseModel():
     def __init__(self, *args, **kwargs):
         """initialize values"""
         if kwargs:
-            for i in kwargs:
-                if i == 'id':
-                    self.id = str(kwargs[i])
-                if i == 'created_at':
-                    var = datetime.strptime(kwargs[i], '%Y-%m-%dT%H:%M:%S.%f')
-                    self.created_at = var
-                if i == 'updated_at':
-                    var = datetime.strptime(kwargs[i], '%Y-%m-%dT%H:%M:%S.%f')
-                    self.updated_at = var
+            kwargs.pop('__class__', None)
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'update_at':
+                    setattr(self, key, datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f'))
+                else:
+                    setattr(self, key, value)
         else:
             # generated a identifier unique and convert in the string
             self.id = str(uuid4())
             # get the date using datatime.now
             self.created_at = datetime.now()
             # update the date
-            self.updated_at = self.created_at()
-        models.storage.new(self)
+            self.updated_at = datetime.now()
+        models.storage.save()
 
     def __str__(self):
         """string representation"""
@@ -39,7 +36,7 @@ class BaseModel():
         models.storage.save()
 
     def to_dict(self):
-        """return a dictionary contain all akeys/values"""
+        """return a dictionary contain all keys/values"""
         dict_aux = self.__dict__.copy()
         dict_aux['__class__'] = self.__class__.__name__
         dict_aux['created_at'] = self.created_at.isoformat()
